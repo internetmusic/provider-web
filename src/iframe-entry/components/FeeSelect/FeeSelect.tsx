591d89be1f69ec068ed4642ffc1e0bd4626c83cb
Viktor Voronin <vvoronin@web3tech.ru>
2020-01-30 10:27:46 +0300

    DEXW-2755: fix fee select

---
+++ src/iframe-entry/components/FeeSelect/FeeSelect.tsx
@@ -0,0 +1,101 @@
+import {
+    Box,
+    BoxProps,
+    FeeOption,
+    List,
+    Select,
+    Selected,
+    Text,
+} from '@waves.exchange/react-uikit';
+import { TLong, TTransactionParamWithType } from '@waves/signer';
+import React, {
+    FC,
+    ReactElement,
+    useCallback,
+    useState,
+    useEffect,
+} from 'react';
+import { IMeta } from '../../services/transactionsService';
+import { assetPropFactory } from '../../utils/assetPropFactory';
+import { getCoins } from '../../utils/math';
+import { getFeeOptions } from './helpers';
+
+export type FeeSelectHandler = (fee: string, feeAssetId: string | null) => void;
+
+type Props = {
+    txMeta: IMeta<TTransactionParamWithType>;
+    fee: TLong;
+    onFeeSelect: FeeSelectHandler;
+    feeAssetId?: string | null;
+    availableWavesBalance: TLong;
+};
+
+export const FeeSelect: FC<Props & BoxProps> = ({
+    txMeta,
+    fee: txFee,
+    feeAssetId,
+    onFeeSelect,
+    availableWavesBalance,
+    ...rest
+}) => {
+    const getAssetProp = assetPropFactory(txMeta.assets);
+    const [feeOptions, setFeeOptions] = useState<FeeOption[]>(
+        getFeeOptions(txFee, txMeta, feeAssetId, availableWavesBalance)
+    );
+
+    useCallback(() => {
+        setFeeOptions(
+            getFeeOptions(txFee, txMeta, feeAssetId, availableWavesBalance)
+        );
+    }, [availableWavesBalance, feeAssetId, txFee, txMeta]);
+
+    const [selectedFeeOption, setSelectedFeeOption] = useState<FeeOption>(
+        feeOptions[0]
+    );
+
+    useEffect(() => {
+        setSelectedFeeOption(feeOptions[0]);
+    }, [feeOptions]);
+
+    const handleFeeSelect = useCallback(
+        (feeOption: FeeOption) => {
+            setSelectedFeeOption(feeOption);
+
+            const feeAssetId = feeOption.id;
+            const fee = getCoins(
+                feeOption.value,
+                getAssetProp(feeOption.id, 'decimals')
+            );
+
+            onFeeSelect(fee, feeAssetId);
+        },
+        [getAssetProp, onFeeSelect]
+    );
+
+    return (
+        <Box {...rest}>
+            <Text variant="body2" color="basic.$500" display="block" mb="$5">
+                Fee
+            </Text>
+            {feeOptions.length > 1 ? (
+                <Box {...rest}>
+                    <Select
+                        placement="top"
+                        renderSelected={(open): ReactElement => (
+                            <Selected
+                                selected={selectedFeeOption}
+                                opened={open}
+                            />
+                        )}
+                    >
+                        <List onSelect={handleFeeSelect} options={feeOptions} />
+                    </Select>
+                </Box>
+            ) : (
+                <Text variant="body2" color="standard.$0">
+                    {selectedFeeOption.value} {selectedFeeOption.name}
+                </Text>
+            )}
+        </Box>
+    );
+};
---
+++ src/iframe-entry/components/FeeSelect/helpers.ts
@@ -0,0 +1,95 @@
+import { TLong, TTransactionParamWithType } from '@waves/signer';
+import BigNumber from '@waves/bignumber';
+import { FeeOption } from '@waves.exchange/react-uikit';
+import { IMeta } from '../../services/transactionsService';
+import { assetPropFactory } from '../../utils/assetPropFactory';
+import { WAVES } from '../../constants';
+import { getPrintableNumber } from '../../utils/math';
+import { DetailsWithLogo } from '../../utils/loadLogoInfo';
+
+export const checkIsEnoughBalance = (balance: TLong, fee: TLong): boolean => {
+    return BigNumber.toBigNumber(balance).gte(
+        BigNumber.toBigNumber(fee).div(Math.pow(10, WAVES.decimals))
+    );
+};
+
+const isFeeAssetId = (
+    feeAssetId: string | null | undefined
+): feeAssetId is string | null => {
+    return typeof feeAssetId !== 'undefined';
+};
+
+const isNonDefaultFeeAssetId = (
+    feeAssetId: string | null | undefined
+): feeAssetId is string => {
+    return typeof feeAssetId !== 'string';
+};
+
+export const formatFee = (fee: TLong, feeAsset: DetailsWithLogo): string =>
+    getPrintableNumber(fee, feeAsset.decimals);
+
+export const getFeeOptions = (
+    txFee: TLong,
+    txMeta: IMeta<TTransactionParamWithType>,
+    txFeeAssetId: string | null | undefined,
+    availableWavesBalance: TLong
+): FeeOption[] => {
+    const getAssetProp = assetPropFactory(txMeta.assets);
+    const wavesFeeOption: FeeOption = {
+        id: null,
+        name: WAVES.name,
+        ticker: WAVES.ticker,
+        value: formatFee(txFee, WAVES),
+    };
+
+    let defaultFeeOption: FeeOption;
+    let feeAsset: DetailsWithLogo;
+
+    if (isFeeAssetId(txFeeAssetId)) {
+        if (isNonDefaultFeeAssetId(txFeeAssetId)) {
+            // case feeAssetId - some asset, but not Waves
+            feeAsset = txMeta.assets[txFeeAssetId];
+            defaultFeeOption = {
+                name: getAssetProp(txFeeAssetId, 'name'),
+                id: txFeeAssetId,
+                ticker: '',
+                value: formatFee(txFee, feeAsset),
+            };
+        } else {
+            // case: feeAssetId - Waves
+            defaultFeeOption = wavesFeeOption;
+        }
+
+        // in case feeAssetId provided we can only use it
+        return [defaultFeeOption];
+    }
+    // case: no feeAssetId provided
+    // feeAsset = WAVES;
+    defaultFeeOption = wavesFeeOption;
+
+    const metaFeeOptions = txMeta.feeList.map((f) => ({
+        name: getAssetProp(f.feeAssetId, 'name'),
+        id: String(f.feeAssetId),
+        ticker: '',
+        value: String(f.feeAmount),
+    }));
+
+    const isEnoughBalance = checkIsEnoughBalance(
+        availableWavesBalance,
+        txFee
+        // feeAsset.decimals
+    );
+    const hasNonDefaultFees = metaFeeOptions.length > 0;
+
+    let feeOptions;
+
+    if (hasNonDefaultFees) {
+        feeOptions = isEnoughBalance
+            ? [defaultFeeOption].concat(metaFeeOptions)
+            : metaFeeOptions;
+    } else {
+        feeOptions = [defaultFeeOption];
+    }
+
+    return feeOptions;
+};
